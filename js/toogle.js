94896dc6cbdb41052a02feda6bfb63c65ee34ab5
hebersonaguiar <hebersonaguiar@gmail.com>
2018-11-07 21:53:34 -0200

    Alterando toggle

--- js/toogle.js
+++ js/toogle.js
@@ -1,8 +1,8 @@
 function toogleFunction() {
-  $('.toggle').click(function(){
+  // $('.toggle').click(function(){
   	alert('Cancel action occurs!');
   //   ('.layout').toggleClass('ativo');
   //   ('.menu-responsivo').toggleClass('ativo');
   //   (this).toggleClass('ativo');
-  });
+  // });
 }
