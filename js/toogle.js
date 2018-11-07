a7ec8795273aec75eff6151b56425502f4f23b11
hebersonaguiar <hebersonaguiar@gmail.com>
2018-11-07 21:14:44 -0200

    Alterando toggle

--- js/toogle.js
+++ js/toogle.js
@@ -1,5 +1,5 @@
 function toogleFunction() {
-  ('.toggle').click(function(){
+  document.getElementByClass("toggle").click(function(){
     ('.layout').toggleClass('ativo');
     ('.menu-responsivo').toggleClass('ativo');
     (this).toggleClass('ativo');
