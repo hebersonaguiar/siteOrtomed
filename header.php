b8c58d45e558fd8d2bb410771e2115aeff3155a2
hebersonaguiar <hebersonaguiar@gmail.com>
2018-11-06 23:05:59 -0200

    Inserindo js file script

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
       <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
       <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
       <script src="<?php bloginfo('template_url'); ?>/js/pace.min.js"></script>
+      <script src="<?php bloginfo('template_url'); ?>/js/toogle.js"></script>
       <link href="<?php bloginfo('template_url'); ?>/css/pace-theme-minimal.css" rel="stylesheet" />
       <link href="<?php bloginfo('template_url'); ?>/css/balloon.css" rel="stylesheet" />
       <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/geral.css">
@@ -34,7 +35,7 @@
          <div class="top-bar boxshadow">
          <div class="container">
             <div class="telefone">	
-               <span>+55 (77) 2101-0500</span><a class="toggle" onclick="toogleFunction()" href="#"><i class="fas fa-bars"></i></a>
+               <span>+55 (77) 2101-0500</span><a class="toggle" onclick="toogleFunction()" href="javascript:toogleFunction();"><i class="fas fa-bars"></i></a>
             </div>
             <div class="social">
                <ul>
