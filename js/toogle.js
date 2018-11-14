66bc0d9e074489a2f566f452c5764ea551e63c4a
hebersonaguiar <hebersonaguiar@gmail.com>
2018-11-14 21:38:05 -0200

    Editando toggle

--- js/toogle.js
+++ js/toogle.js
@@ -1,6 +1,16 @@
 function toogleFunction() {
-	var element = document.getElementById("responsivo");
-	element.classList.add("ativo");
+	// var element = document.getElementById("responsivo");
+	// element.classList.add("ativo");
+	if (!document.getElementsByClassName("ativo").length){
+	    //class not there
+	    var element = document.getElementById("responsivo");
+		element.classList.add("ativo");
+	}
+	else{
+	//class there
+		var element = document.getElementById("responsivo");
+    	element.classList.remove("ativo");
+	}
   // $('.toggle').click(function(){
   	// alert('Cancel action occurs!');
   //   ('.layout').toggleClass('ativo');
