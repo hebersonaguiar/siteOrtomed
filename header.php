c60c6b474fb16f37f0762ae894b5e453cf2f3017
hebersonaguiar <hebersonaguiar@gmail.com>
2018-11-14 22:26:13 -0200

    Alterando rodape

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
       <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/geral.css">
       <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/plugins.css">      
       <script src="<?php bloginfo('template_url'); ?>/js/pace.min.js"></script>
+      <script src="<?php bloginfo('template_url'); ?>/js/toogle.js"></script>
       <link href="<?php bloginfo('template_url'); ?>/css/pace-theme-minimal.css" rel="stylesheet" />
       <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/<?php echo $style; ?>.css">
       <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.css">
--- js/toogle.js
+++ js/toogle.js
@@ -1,19 +1,37 @@
-function toogleFunction() {
-	// var element = document.getElementById("responsivo");
-	// element.classList.add("ativo");
-	if (!document.getElementsByClassName('ativo').length){
-	    //class not there
-	}
-	else{
-	//class there
-		var element = document.getElementById("bar");
-    	element.classList.remove("fas fa-bars");
-    	element.classList.add("fa fa-envelope");
-	}
-  // $('.toggle').click(function(){
-  	// alert('Cancel action occurs!');
-  //   ('.layout').toggleClass('ativo');
-  //   ('.menu-responsivo').toggleClass('ativo');
-  //   (this).toggleClass('ativo');
-  // });
-}
+// When the user scrolls down 20px from the top of the document, show the button
+   window.onscroll = function() {scrollFunction()};
+   
+   function scrollFunction() {
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+           document.getElementById("myBtn").style.display = "block";
+       } else {
+           document.getElementById("myBtn").style.display = "none";
+       }
+   }
+   
+   // When the user clicks on the button, scroll to the top of the document
+   function topFunction() {
+       document.body.scrollTop = 0;
+       document.documentElement.scrollTop = 0;
+   }
+   //Page is loading start progress
+  document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+      NProgress.start(); //start progress bar
+    }
+  }
+
+  //when page is loaded stop progress 
+  var everythingLoaded = setInterval(function() {
+    if (/loaded|complete/.test(document.readyState)) {
+      clearInterval(everythingLoaded);
+      setTimeout(function(){NProgress.done()},2000);
+    }
+  }, 10);
+  $(document).ready(function() {
+    NProgress.start();
+  });
+  $(window).load(function() {
+      //setTimout() for 2 seconds progress stop delay
+      setTimeout(function(){NProgress.done()},2000);
+  });
