85404571fc24a038f40a72b52401dee9ba63954b
hebersonaguiar <hebersonaguiar@gmail.com>
2019-01-25 20:23:40 -0200

    Corrigindo Back to TOp

--- includes/pages/especialidades.php
+++ includes/pages/especialidades.php
@@ -1,65 +1,71 @@
 <div class="container">
-	<section class="sec-especialidades">
-		<div class="especialidades">
-			<table>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#"  data-balloon-length="medium" data-balloon-pos="left" data-balloon="Vivian Souza Ferraz">Acupuntura</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="fit" data-balloon-pos="right" data-balloon="Sifredo Pedral S. Neto Sidineia Santos da Rocha">Angiologia/Cirurgia Vascular</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i><a href="#" data-balloon-length="medium" data-balloon-pos="left" data-balloon="Tais de lima e fraga"> Buco-maxilofacial e Implantodontia</a></td>
-			    <td><i class="fas fa-user-md"></i><a href="#" data-balloon-length="medium" data-balloon-pos="right" data-balloon="Tais de lima e fraga"> Cirurgia e Traumatologia</a></td>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Fernanda Sales Correia, Vivian Souza Ferraz">Clínica Médica</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Fabiola Cristina da Silva Fraga">Dermatologia</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Lauro jose ribeiro viana">Endrocrinologia</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="medium" data-balloon-pos="right" data-balloon="Newiton Monteiro">Fisioterapia e Reabilitação</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Antonio Ribeiro Correia, Argemiro Correia Santos Júnior">Medicina do Trabalho</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Daniele Sales Alves Correia">Neurologia</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Aline dos Santos Rocha">Nutrição</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Antonio Ribeiro Correia, Argemiro Correia Santos Júnior, Umberto Paulo de Castro Alves Filho, Valdemario de Magalhaes Fraga">Ortopedia e Traumatologia</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Valdemario de Magalhaes Fraga">Ortopedia Pediátrica</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Umberto Paulo de Castro Alves Filho">Ortopedia de Joelho</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="medium" data-balloon-pos="left" data-balloon="Antonio José Pinho Iza Virginia Alves Pinheiro Rafaela Oliveira">Psicologia</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Ednea Conceição Correia Luz, Ciro Novais Faria e Silva">Psiquiatria</a></td>
-			  </tr>
-			  <tr>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Cassio Lamego Bulos, Igor Aloisio Carcez Zamilute">Radiologia e Ultrassonografia</a></td>
-			    <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Fernanda Sales Correia">Reumatologia</a></td>
-			  </tr>
-			</table>
-		</div>
-	</section>
+   <section class="sec-especialidades">
+      <div class="especialidades">
+         <table>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#"  data-balloon-length="medium" data-balloon-pos="left" data-balloon="Vivian Souza Ferraz">Acupuntura</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="fit" data-balloon-pos="right" data-balloon="Sifredo Pedral S. Neto Sidineia Santos da Rocha">Angiologia/Cirurgia Vascular</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i><a href="#" data-balloon-length="medium" data-balloon-pos="left" data-balloon="Tais de lima e fraga"> Buco-maxilofacial e Implantodontia</a></td>
+               <td><i class="fas fa-user-md"></i><a href="#" data-balloon-length="medium" data-balloon-pos="right" data-balloon="Tais de lima e fraga"> Cirurgia e Traumatologia</a></td>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Fernanda Sales Correia, Vivian Souza Ferraz">Clínica Médica</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Fabiola Cristina da Silva Fraga">Dermatologia</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Lauro jose ribeiro viana">Endrocrinologia</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="medium" data-balloon-pos="right" data-balloon="Newiton Monteiro">Fisioterapia e Reabilitação</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Antonio Ribeiro Correia, Argemiro Correia Santos Júnior">Medicina do Trabalho</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Daniele Sales Alves Correia">Neurologia</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Aline dos Santos Rocha">Nutrição</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Antonio Ribeiro Correia, Argemiro Correia Santos Júnior, Umberto Paulo de Castro Alves Filho, Valdemario de Magalhaes Fraga">Ortopedia e Traumatologia</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Valdemario de Magalhaes Fraga">Ortopedia Pediátrica</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Umberto Paulo de Castro Alves Filho">Ortopedia de Joelho</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="medium" data-balloon-pos="left" data-balloon="Antonio José Pinho Iza Virginia Alves Pinheiro Rafaela Oliveira">Psicologia</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Ednea Conceição Correia Luz, Ciro Novais Faria e Silva">Psiquiatria</a></td>
+            </tr>
+            <tr>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="left" data-balloon="Cassio Lamego Bulos, Igor Aloisio Carcez Zamilute">Radiologia e Ultrassonografia</a></td>
+               <td><i class="fas fa-user-md"></i>   <a href="#" data-balloon-length="large" data-balloon-pos="right" data-balloon="Fernanda Sales Correia">Reumatologia</a></td>
+            </tr>
+         </table>
+      </div>
+   </section>
 </div>
 </main>	
 <main class="second-main">
-<div class="container">
-	<section class="sec-dep">
-	</section>
-</div>
+   <div class="container">
+      <section class="sec-dep">
+      </section>
+   </div>
 </main>
 <main class="third-main">
 <section class="sec-medias">
-	<div class="container">
-		<div class="iframe-face">
-			<span>facebook</span>
-			<div class="clear"></div>
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-			<div class="iframe-insta">
-				<span>instagram</span>
-				<div class="clear"></div>
-				<blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p></div></blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="iframe-face">
+         <span>facebook</span>
+         <div class="clear"></div>
+         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+         <div class="iframe-insta">
+            <span>instagram</span>
+            <div class="clear"></div>
+            <blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;">
+               <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
+                  <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div>
+               </div>
+               <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p>
+         </div>
+         </blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
+      </div>
+   </div>
+   </div>
 </section>
