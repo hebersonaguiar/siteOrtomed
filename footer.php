d67780f9517f426afaa2092257e1bf58c24b0ff6
hebersonaguiar <hebersonaguiar@gmail.com>
2019-01-25 20:52:19 -0200

    Corrigindo Top

--- footer.php
+++ footer.php
@@ -31,46 +31,24 @@
 </footer>
 </div>
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-<script src="<?php bloginfo('template_url'); ?>/bower_components/jquery/dist/jquery.min.js"></script>
 <script>
-   // When the user scrolls down 20px from the top of the document, show the button
-   window.onscroll = function() {scrollFunction()};
-   
-   function scrollFunction() {
-       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-           document.getElementById("myBtn").style.display = "block";
-       } else {
-           document.getElementById("myBtn").style.display = "none";
-       }
-   }
-   
-   // When the user clicks on the button, scroll to the top of the document
-   function topFunction() {
-       document.body.scrollTop = 0;
-       document.documentElement.scrollTop = 0;
-   }
-   //Page is loading start progress
-  document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-      NProgress.start(); //start progress bar
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
     }
-  }
-
-  //when page is loaded stop progress 
-  var everythingLoaded = setInterval(function() {
-    if (/loaded|complete/.test(document.readyState)) {
-      clearInterval(everythingLoaded);
-      setTimeout(function(){NProgress.done()},2000);
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        // document.documentElement.scrollDuration = 700;
     }
-  }, 10);
-  $(document).ready(function() {
-    NProgress.start();
-  });
-  $(window).load(function() {
-      //setTimout() for 2 seconds progress stop delay
-      setTimeout(function(){NProgress.done()},2000);
-  });
 </script>
+<script src="<?php bloginfo('template_url'); ?>/bower_components/jquery/dist/jquery.min.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
